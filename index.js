2310109468e1d712adf1d26fbaf53024cdd1294b
akshat1423 <125379408+akshat1423@users.noreply.github.com>
2023-08-05 14:25:59 +0530

    Update index.js 9

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+const openai = require('openai');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
@@ -24,6 +30,67 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
+
+const accountSid = 'ACa11021a29ca5245213d79d5b4b970fa3';
+const authToken = '7fa2630caa69e36b764da90cb1c0f28c';
+const client = new twilio(accountSid, authToken);
+
+openai.api_key = "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";
+
+function getCompletion(prompt, model = "gpt-3.5-turbo") {
+    const messages = [{ role: "user", content: prompt }];
+    const response = openai.ChatCompletion.create({
+        model: model,
+        messages: messages,
+        temperature: 0,
+    });
+    return response.choices[0].message.content;
+}
+
+function getCompletionFromMessages(messages, model = "gpt-3.5-turbo", temperature = 0, max_tokens = 500) {
+    const response = openai.ChatCompletion.create({
+        model: model,
+        messages: messages,
+        temperature: temperature,
+        max_tokens: max_tokens,
+    });
+    return response.choices[0].message.content;
+}
+
+app.post('/whatsapp', async (req, res) => {
+    const messages = [
+        {
+            role: 'system',
+            content: `You are an ancient medicine practitioner , your skills include Ayurvedic Doctor , Yoga Teacher, Nutritionalist / Dietician , Gym Trainer . You have to decide which among your skills can suit to best help your clients problem , give him herbal and natural solutions not having to use any artificial medicines.`
+        },
+        {
+            role: 'user',
+            content: req.body.Body // Assuming the incoming message is in the 'Body' field
+        }
+    ];
+
+    const response = getCompletionFromMessages(messages, temperature = 1);
+    const chunkSize = 1200;
+    const stringChunks = splitString(response, chunkSize);
+
+    client.messages
+        .create({
+            from: 'whatsapp:+14155238886',
+            body: stringChunks,
+            to: `whatsapp:${req.body.From}` // Assuming the sender's phone number is in the 'From' field
+        })
+        .then(message => console.log(message.sid))
+        .catch(error => console.error(error));
+
+    res.send('Message sent');
+});
+function splitString(text, chunkSize) {
+    const chunks = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+        chunks.push(text.slice(i, i + chunkSize));
+    }
+    return chunks;
+}
 
 mongoose.connect('mongodb+srv://AxeAdmin:dwwePT1tWk6KXxEm@axechat.xaodyf9.mongodb.net/?retryWrites=true&w=majority');
 
@@ -42,14 +109,15 @@ app.post('/register', async (req,res) => {
 });
 
 
-app.post('/whatsapp', async (req, res) => {
-  const postedData = req.body;
+// app.post('/whatsapp', async (req, res) => {
+//   const postedData = req.body;
 
-  // Construct the plain text response
-  const textResponse = JSON.stringify(postedData, null, 2);
+//   // Construct the plain text response
+//   const textResponse = JSON.stringify(postedData, null, 2);
   
-  res.send(textResponse);
-});
+//   res.send(textResponse);
+// });
+
 app.post('/login',   async (req,res) => {
   const {username,password} = req.body;
   const userDoc = await User.findOne({username});
